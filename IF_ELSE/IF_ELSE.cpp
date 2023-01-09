cd6077db8adf73c70a560159f6dd2dbe21c99f0f
Dmitriy <maradonna05@yandex.ru>
2023-01-09 12:01:38 +0300

    Добавьте файлы проекта.

---
+++ IF_ELSE/IF_ELSE.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Vvedite a: ");
+    scanf("%i",&a);
+    printf("Vvedite b: ");
+    scanf("%i", &b);
+    if (a > b)
+    {
+        printf("a bolshe!");
+    }
+    else
+    {
+        if (a == b)
+        {
+            printf("a ravno b!");
+        }
+        else
+        {
+            printf("b bolshe!");
+        }
+    }
+}
